2da2ae8996b53e4567009031f546cf816ce12f59
Tiane Maestas <77022786+Tiane-Maestas@users.noreply.github.com>
2021-12-26 13:39:31 -0800

    Quick Upload from PC of working projects

---
+++ MusicBotV1.py
@@ -0,0 +1,157 @@
+import discord
+from discord.ext import commands
+import pafy
+import os
+import time
+
+token = "ODg3Nzc2NjE3ODg2Mjg1ODg0.YUJEZw.I-RVoiOm2uVQS-gfw3DGcUFcrZY"
+client = commands.Bot(command_prefix = "-")
+audio_queue = []
+
+#works
+@client.event
+async def on_ready():
+    print("Bot Online!")
+
+@client.command(aliases=['paly', 'queue'])
+async def play(ctx, url : str):
+
+    if ctx.author.voice:
+        voice_client_test = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        if voice_client_test == None:
+            voice_channel = ctx.message.author.voice.channel
+            await voice_channel.connect()
+    else:
+        await ctx.send("Please join a Voice Channel.")
+        return
+
+    audio_queue.append(url)
+    
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing() or voice.is_paused():
+        await ctx.send("To play inputed audio 'skip' the current audio.")
+        return
+
+    audio_isthere = os.path.isfile("audio.webm")
+    if audio_isthere:
+        os.remove("audio.webm")        
+
+    #downloads the url audio in WEBM format
+    video = pafy.new(audio_queue[0])
+    audio_queue.pop(0)
+    audio = video.getbestaudio(preftype="webm",ftypestrict=True)
+    audio.download()
+
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            print("Playing:")
+            print(file)
+            print()
+            os.rename(file, "audio.webm")
+    
+    audio_source = discord.FFmpegPCMAudio("audio.webm")
+    voice.play(audio_source)
+
+@client.command()
+async def skip(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing() or voice.is_paused():
+        if len(audio_queue) != 0:
+            voice.stop()
+            time.sleep(1)
+            os.remove("audio.webm")
+
+            video = pafy.new(audio_queue[0])
+            audio_queue.pop(0)
+            audio = video.getbestaudio(preftype="webm",ftypestrict=True)
+            audio.download()
+
+            for file in os.listdir("./"):
+                if file.endswith(".webm"):
+                    print("Playing:")
+                    print(file)
+                    print()
+                    os.rename(file, "audio.webm")
+    
+            audio_source = discord.FFmpegPCMAudio("audio.webm")
+            voice.play("audio.webm")
+        else:
+            await ctx.send("No Songs In Queue.")
+            return
+
+@client.command()
+async def next(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    print("Current Queue:")
+    print(audio_queue)
+    print()
+
+    if len(audio_queue) == 0:
+        await ctx.send("No Songs In Queue.")
+        return
+    if voice.is_playing() or voice.is_paused():
+        await ctx.send("Please 'skip' the current audio.")
+        return
+    
+    audio_isthere = os.path.isfile("audio.webm")
+    if audio_isthere:
+        os.remove("audio.webm")
+
+    video = pafy.new(audio_queue[0])
+    audio_queue.pop(0)
+    audio = video.getbestaudio(preftype="webm",ftypestrict=True)
+    audio.download()
+
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            print("Playing:")
+            print(file)
+            print()
+            os.rename(file, "audio.webm")
+    
+    audio_source = discord.FFmpegPCMAudio("audio.webm")
+    voice.play(audio_source)
+    
+#works
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice != None:
+        await voice.disconnect()
+    else:
+        await ctx.send("Not in any voice channel.")
+
+#works
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("No audio is playing.")
+
+#works
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("Audio is playing.")
+
+#works
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+    time.sleep(1)
+
+#works
+@client.command()
+async def shutdown(ctx):
+    client.close()
+    time.sleep(1)
+    quit()
+
+client.run(token)
---
+++ MusicBotV2.py
@@ -0,0 +1,118 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
+                  'options': '-vn'}
+YDL_OPTIONS = {'format': 'bestaudio'}
+#36768768 for permissions
+
+token = "ODg3Nzc2NjE3ODg2Mjg1ODg0.YUJEZw.I-RVoiOm2uVQS-gfw3DGcUFcrZY"
+client = commands.Bot(command_prefix = "-")
+
+audio_queue = []
+
+#works
+@client.event
+async def on_ready():
+    print("Bot Online!")
+
+#works
+async def Stream_Audio(voice, ctx):
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        url = audio_queue[0]
+        audio_queue.pop(0)
+        print("Playing:")
+        print(url)
+        audio_info = ydl.extract_info(url, download=False)
+        stream_url = audio_info['formats'][0]['url']
+        audio_source = await discord.FFmpegOpusAudio.from_probe(stream_url,
+        **FFMPEG_OPTIONS)
+        voice.play(audio_source)
+        message = "Currently Playing: " + url
+        await ctx.send(message)
+        print()
+
+#works
+@client.command(aliases=['paly', 'queue'])
+async def play(ctx, url : str):
+
+    if ctx.author.voice:
+        voice_client_test = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        if voice_client_test == None:
+            voice_channel = ctx.message.author.voice.channel
+            await voice_channel.connect()
+    else:
+        await ctx.send("Please join a Voice Channel.")
+        return
+
+    audio_queue.append(url)
+    
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing() or voice.is_paused():
+        await ctx.send("To play inputed audio 'skip' the current audio.")
+        return        
+
+    await Stream_Audio(voice, ctx)
+
+#works
+@client.command(aliases=['next'])
+async def skip(ctx):
+    print("Current Queue:")
+    print(audio_queue)
+    print()
+
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    if len(audio_queue) == 0:
+        await ctx.send("No Song In Queue.")
+        return
+
+    if voice.is_playing() or voice.is_paused():
+        voice.stop()
+        await Stream_Audio(voice, ctx)
+        return
+
+    await Stream_Audio(voice, ctx)
+    
+#works
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice != None:
+        await voice.disconnect()
+    else:
+        await ctx.send("Not in any voice channel.")
+
+#works
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("No audio is playing.")
+
+#works
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("Audio is playing.")
+
+#works
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+#works
+@client.command()
+async def shutdown(ctx):
+    await client.close()
+    quit(0)
+
+client.run(token)
